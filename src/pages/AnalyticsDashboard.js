ce25e01559fdd7187d23b98ab8dcbc1df69d0e08
awkperl <infinixhp9548@gmail.com>
2026-08-11 12:33:32 +0300

    analytics

---
+++ src/pages/AnalyticsDashboard.js
@@ -0,0 +1,15 @@
+import AnalyticsOverview from "../components/AnalyticsOverview";
+
+export default function AnalyticsDashboard({ token }) {
+
+    return (
+
+        <div>
+
+            <AnalyticsOverview token={token} />
+
+        </div>
+
+    );
+
+}
